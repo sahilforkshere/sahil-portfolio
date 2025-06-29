c12b91b3742cd9299fdec8a6de4da03e91e78de2
Sahil <paalsahil04@gmail.com>
2025-06-30 03:09:00 +0530

    changes for production

--- src/components/heroSection/Heropic.jsx
+++ src/components/heroSection/Heropic.jsx
@@ -14,7 +14,7 @@ const Heropic = () => {
       viewport={{ once: false, amount: 0 }}
       className=" h-full flex items-center justify-center "
     >
-      <img src="../../../public/images/sahil_pic.jpg" alt="sahil" className="max-h-[450px] w-auto" />
+      <img src="/images/sahil_pic.jpg" alt="sahil" className="max-h-[450px] w-auto" />
 
       <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
