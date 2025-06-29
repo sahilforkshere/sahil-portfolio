62c13164283a8f467dde101e3e072c246b5bb17e
Sahil <paalsahil04@gmail.com>
2025-06-30 02:47:13 +0530

    no

--- src/components/aboutMeSection/AboutMeMain.jsx
+++ src/components/aboutMeSection/AboutMeMain.jsx
@@ -11,7 +11,7 @@ const AboutMeMain = () => {
       className="flex md:flex-row sm:flex-col gap-12 px-4 max-w-[1200px] mx-auto mt-[100px] justify-between items-center"
     >
       <motion.div
-        variants={fadeIn("right", 0)}
+        variants={fadeIn("right ", 0)}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.7 }}
